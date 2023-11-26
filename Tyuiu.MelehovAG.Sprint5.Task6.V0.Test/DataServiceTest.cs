0218d857443c1a383a5a762170226f994f733f4d
Tyuiu.MelehovAG@outlook.com <dedmeleh@gmail.com>
2023-11-27 01:02:55 +0500

    Task4

--- Tyuiu.MelehovAG.Sprint5.Task6.V0.Test/DataServiceTest.cs
+++ Tyuiu.MelehovAG.Sprint5.Task6.V0.Test/DataServiceTest.cs
@@ -11,6 +11,12 @@ namespace Tyuiu.MelehovAG.Sprint5.Task6.V0.Test
         [TestMethod]
         public void CheckExistsFile()
         {
+            string path = @"C:\Users\Иридий\source\repos\Tyuiu.MelehovAG.Sprint5\Tyuiu.MelehovAG.Sprint5.Task6.V0\bin\Debug\InPutDataFileTask6V0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
